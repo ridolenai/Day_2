651cd2d75d09edf65bca447e3291bf85b20fcf42
ridolenai <92106359+ridolenai@users.noreply.github.com>
2021-10-08 11:10:11 -0500

    Initial Commit

---
+++ worksheet day 2.py
@@ -0,0 +1,48 @@
+
+
+# for number in range (5):
+#     print ("hello")
+
+
+# for number in range (11):
+#     print (number)
+
+# for number in range (10,-1,-1):
+#     print (number)
+
+# run_quantity = int(input("How many times would you like to see it? "))
+# for count in range (run_quantity):
+#     print("devCodeCamp")
+
+# trash_football = 'packers'
+# for letter in trash_football:
+#      print (letter)
+
+#
+# for number in range (101):
+#     if number % 3 == 0 and number % 5 == 0:
+#         print ('fizzbuzz')
+#     elif number % 3 == 0:
+#         print ('fizz')
+#     elif number % 5 == 0:
+#         print ('buzz')
+#     else:
+#         print (number)
+#
+#
+# i = 0
+# while i < 5:
+#       i += 1
+#       print ('Hello')
+#
+#
+
+   
+# password = 'foosballisthedevil'
+# guess = (input("Type your password: "))
+# while guess != password:
+#    guess = (input("That password is incorrect. Try again: "))
+#
+# if guess == password:
+    print("User Validated")
+
